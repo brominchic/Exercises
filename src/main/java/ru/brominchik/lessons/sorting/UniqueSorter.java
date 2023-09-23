10ada26acf0c76f713afa846a84e33ceb570a49d
Тимур <timurpetrov1875@gmail.com>
2023-09-23 14:25:58 +0300

    Scratch-2

---
+++ src/main/java/ru/brominchik/lessons/sorting/UniqueSorter.java
@@ -0,0 +1,21 @@
+package ru.brominchik.lessons.sorting;
+
+import java.util.LinkedList;
+import java.util.List;
+
+class UniqueSorter {
+
+    public static List<Integer> sort(int[] firstArr, int[] lastArr) {
+        List<Integer> finalList = new LinkedList<>();
+        int n = 0;
+        for (int j : firstArr) {
+            if (j == lastArr[n]) {
+                n = n + 1;
+            } else {
+                finalList.add(j);
+            }
+        }
+        return finalList;
+    }
+
+}
--- src/test/java/ru/brominchik/lessons/sorting/UniqueSorterTest.java
+++ src/test/java/ru/brominchik/lessons/sorting/UniqueSorterTest.java
@@ -1,35 +1,40 @@
+package ru.brominchik.lessons.sorting;
+
 import org.junit.jupiter.api.Test;
+import ru.brominchik.lessons.sorting.UniqueSorter;
 
 import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ScratchTest {
+public class UniqueSorterTest {
 
     @Test
     public void scratchTest() {
-        Scratch scratch = new Scratch();
-        LinkedList linkedList = new LinkedList<>();
+        List<Integer> linkedList = new LinkedList<>();
         linkedList.add(1);
         linkedList.add(2);
         linkedList.add(8);
         int[] a = {1, 2, 3, 4, 8};
         int[] b = {3, 4, 5};
-        LinkedList list = Scratch.sort(a, b);
-        assertTrue(list.equals(linkedList));
+        List<Integer> list = UniqueSorter.sort(a, b);
+        assertEquals(list, linkedList);
     }
 
     @Test
     void secondScratchTest() {
-        Scratch scratch = new Scratch();
-        LinkedList linkedList = new LinkedList<>();
+        List<Integer> linkedList = new LinkedList<>();
         linkedList.add(1);
         linkedList.add(2);
+        linkedList.add(5);
+        linkedList.add(9);
         int[] a = {1, 2, 3, 4, 5, 9};
         int[] b = {3, 4, 8};
-        LinkedList list = Scratch.sort(a, b);
+        List<Integer> list = UniqueSorter.sort(a, b);
         System.out.println(list);
+        assertEquals(list, linkedList);
     }
 
     // подумай, эффективно ли твое решение в этом случае
@@ -45,7 +50,7 @@ public class ScratchTest {
             second[i - 100] = i;
         }
 
-        var result = Scratch.sort(first, second);
+        var result = UniqueSorter.sort(first, second);
         assertEquals(100, result.size());
     }
 
@@ -62,7 +67,7 @@ public class ScratchTest {
             first[i] = i;
         }
 
-        var result = Scratch.sort(first, second);
+        var result = UniqueSorter.sort(first, second);
         assertEquals(100, result.size());
     }
 }
