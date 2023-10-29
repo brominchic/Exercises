cfb5eae8464684dc5a0f7f5395c5cf7fad8d43b0
brominchic <132040687+brominchic@users.noreply.github.com>
2023-10-29 23:53:46 +0500

    Задание 3 Atomic счетчик
    * Выполнил задание по счетчику
    
    * Прибрал код
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Отработал по коментариям
    
    * Вернул тест
    
    * Изменил increase
    
    * добавил логи
    
    * вернул возвращение true/false
    
    * Мелкие доработки
    
    ---------
    
    Co-authored-by: Тимур <timurpetrov1875@gmail.com>

---
+++ src/main/java/ru/brominchik/lessons/threads/counter/atomic/Incrementer.java
@@ -0,0 +1,45 @@
+package ru.brominchik.lessons.threads.counter.atomic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Incrementer implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(Incrementer.class);
+    private final AtomicInteger atomicInteger;
+    protected final String name;
+    private final Map<Integer, String> mapOfValues;
+    private final int finalNumber;
+
+    public Incrementer(AtomicInteger atomicInteger, String name, Map<Integer, String> mapOfValues, int finalNumber) {
+        this.atomicInteger = atomicInteger;
+        this.name = name;
+        this.mapOfValues = mapOfValues;
+        this.finalNumber = finalNumber;
+
+    }
+
+    @Override
+    public void run() {
+        while (atomicInteger.intValue() < finalNumber) {
+            int expectedValue = atomicInteger.intValue();
+            if (expectedValue < finalNumber) {
+                increase(expectedValue);
+            }
+        }
+
+    }
+
+    private boolean increase(int expectedValue) {
+        if (atomicInteger.compareAndSet(expectedValue, expectedValue + 1)) {
+            mapOfValues.put(atomicInteger.intValue(), " Я " + name + " выхватил значение " + (expectedValue + 1));
+            logger.info(" Я " + name + " выхватил значение " + (expectedValue + 1));
+            return true;
+        }
+        return false;
+    }
+}
+
+
---
+++ src/test/java/ru/brominchik/lessons/threads/counter/atomic/AtomicCounterTest.java
@@ -0,0 +1,53 @@
+package ru.brominchik.lessons.threads.counter.atomic;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.lang.Thread.sleep;
+
+public class AtomicCounterTest {
+    private static final Logger logger = LoggerFactory.getLogger(AtomicCounterTest.class);
+
+    @Test
+    public void test() throws InterruptedException {
+        var threadPool = Executors.newFixedThreadPool(5);
+        Map<Integer, String> mapOfValues = new ConcurrentHashMap<>();
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        List<Runnable> listForRunnable = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            listForRunnable.add(new Incrementer(atomicInteger, " номер " + i, mapOfValues, 30));
+        }
+        listForRunnable.forEach(threadPool::submit);
+        sleep(100);
+        for (Map.Entry<Integer, String> pair : mapOfValues.entrySet()) {
+            String value = pair.getValue();
+            logger.info(value);
+        }
+    }
+
+    @Test
+    void testForSomeAmount() throws InterruptedException {
+        List<Thread> threads = new ArrayList<>();
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        Map<Integer, String> mapOfValues = new ConcurrentHashMap<>();
+        for (int i = 0; i < 5; i++) {
+            threads.add(new Thread(new Incrementer(atomicInteger, " номер " + i, mapOfValues, 30)));
+            threads.get(i).start();
+        }
+        for (int i = 0; i < 5; i++) {
+            threads.get(i).join();
+        }
+        for (Map.Entry<Integer, String> pair : mapOfValues.entrySet()) {
+            String value = pair.getValue();
+            logger.info(value);
+        }
+    }
+}
