7f233ae31ea92a48dc022b98dff935341f12a3b9
Тимур <timurpetrov1875@gmail.com>
2023-10-29 18:28:48 +0500

    добавил логи

--- src/main/java/ru/brominchik/lessons/threads/counter/atomic/Incrementer.java
+++ src/main/java/ru/brominchik/lessons/threads/counter/atomic/Incrementer.java
@@ -1,5 +1,8 @@
 package ru.brominchik.lessons.threads.counter.atomic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -8,12 +11,15 @@ public class Incrementer implements Runnable {
     protected final String name;
     private final Map<Integer, String> mapOfValues;
     private final int finalNumber;
+    private static final Logger logger = LoggerFactory.getLogger(Incrementer.class);
+
 
     public Incrementer(AtomicInteger atomicInteger, String name, Map<Integer, String> mapOfValues, int finalNumber) {
         this.atomicInteger = atomicInteger;
         this.name = name;
         this.mapOfValues = mapOfValues;
         this.finalNumber = finalNumber;
+
     }
 
     @Override
@@ -27,12 +33,12 @@ public class Incrementer implements Runnable {
 
     }
 
-    private boolean increase(int expectedValue) {
+    private void increase(int expectedValue) {
         if (atomicInteger.compareAndSet(expectedValue, expectedValue + 1)) {
             mapOfValues.put(atomicInteger.intValue(), " Я " + name + " выхватил значение " + (expectedValue + 1));
-            return true;
+            logger.info(" Я " + name + " выхватил значение " + (expectedValue + 1));
         }
-        return false;
+
     }
 }
 
