6c3c2c052011275199d2b6f9db8b288b70f6f3f1
Тимур <timurpetrov1875@gmail.com>
2023-11-01 20:10:44 +0500

    Работа с файлами

--- src/main/java/ru/brominchik/lessons/files/ModsSwitcher.java
+++ src/main/java/ru/brominchik/lessons/files/ModsSwitcher.java
@@ -2,19 +2,74 @@ package ru.brominchik.lessons.files;
 
 import javax.swing.filechooser.FileSystemView;
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class ModsSwitcher {
+    static Scanner in = new Scanner(System.in);
 
-    public static void main(String arg[]) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
         discs();
         System.out.println("Выберите диск");
         String disk = in.nextLine();
         File folder = new File(disk + ":");
         pathTo(folder);
+
+    }
+
+    public static void discs() {
+        File[] paths;
+        FileSystemView fsv = FileSystemView.getFileSystemView();
+        paths = File.listRoots();
+        for (File path : paths) {
+            System.out.println("Drive Name: " + path);
+            System.out.println("Description: " + fsv.getSystemTypeDescription(path));
+        }
+    }
+
+    public static void pathTo(File folder) throws IOException {
+        Map<Integer, File> values = new HashMap<>();
+        Scanner in = new Scanner(System.in);
+        int i = 0;
+        try {
+            for (File file : folder.listFiles()) {
+                values.put(i, file);
+                i++;
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Такого диска не существует");
+            String[] string = new String[0];
+            main(string);
+        }
+
+        for (Map.Entry<Integer, File> entry : values.entrySet()) {
+            Integer key = entry.getKey();
+
+            String value = entry.getValue().getName();
+            System.out.println(key + " - " + value);
+        }
+        System.out.println(values.size() + " - " + "Назад");
+        System.out.println("Выберите папку");
+        int num = in.nextInt();
+        try {
+            if (num == values.size()) {
+                String[] string = new String[0];
+                main(string);
+            } else {
+                if (values.get(num).isDirectory()) {
+                    if ((values.get(num).listFiles()[0].getName().equals("Installers")) & (values.get(num).listFiles()[1].getName().equals("menyooStuff")))
+                        EnableDisable(values.get(num));
+                }
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Такой папки нет");
+            pathTo(folder);
+        }
+    }
+
+    public static void EnableDisable(File folder) {
         System.out.println("Включить/Отключить");
         String nextLine = in.nextLine();
 
@@ -25,9 +80,9 @@ public class ModsSwitcher {
                     return;
                 }
                 if (file.getName().equals("modsgg")) {
-                    String path = "F:\\Grand Theft Auto V\\modsgg";
-                    File modsFile = new File(path);
-                    modsFile.renameTo(new File("F:\\Grand Theft Auto V\\mods"));
+                    String modsPath = folder.getAbsolutePath() + "\\modsgg";
+                    File modsFile = new File(modsPath);
+                    modsFile.renameTo(new File(folder.getAbsolutePath() + "\\mods"));
                     modsFile.delete();
                     System.out.println("Моды Включены");
                     return;
@@ -41,50 +96,17 @@ public class ModsSwitcher {
                     return;
                 }
                 if (file.getName().equals("mods")) {
-                    String path = "F:\\Grand Theft Auto V\\mods";
-                    File modsFile = new File(path);
-                    modsFile.renameTo(new File("F:\\Grand Theft Auto V\\modsgg"));
+                    String modsPath = folder.getAbsolutePath() + "\\mods";
+                    File modsFile = new File(modsPath);
+                    modsFile.renameTo(new File(folder.getAbsolutePath() + "\\modsgg"));
                     modsFile.delete();
                     System.out.println("Моды Выключены");
                     return;
                 }
             }
-        }
-    }
-
-    public static void discs() {
-        File[] paths;
-        FileSystemView fsv = FileSystemView.getFileSystemView();
-        paths = File.listRoots();
-        for (File path : paths) {
-
-            System.out.println("Drive Name: " + path);
-            System.out.println("Description: " + fsv.getSystemTypeDescription(path));
-        }
-    }
-
-    public static String pathTo(File folder) {
-        Map<Integer, String> values = new HashMap<>();
-        Scanner in = new Scanner(System.in);
-        int i = 0;
-        for (File file : folder.listFiles()) {
-            values.put(i, file.getName());
-            i++;
-            System.out.println(file.getName());
-        }
-        for (Map.Entry<Integer, String> entry : values.entrySet()) {
-            Integer key = entry.getKey();
-
-            String value = entry.getValue();
-            System.out.println(key + " - " + value);
-        }
-        System.out.println("Выберите папку");
-        int num = in.nextInt();
-        if (values.get(num).equals(null)) {
-            System.out.println("Такой папки нет");
         } else {
-            
+            System.out.println("Команда не распознана");
+            EnableDisable(folder);
         }
-        return "1";
     }
 }
