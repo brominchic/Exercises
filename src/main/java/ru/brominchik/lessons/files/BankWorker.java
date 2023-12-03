5db6aec5d9042eeeb26eedb0977d44acd92ed8ce
Тимур <timurpetrov1875@gmail.com>
2023-12-03 19:35:54 +0300

    Переработка тестов

--- src/main/java/ru/brominchik/lessons/files/BankWorker.java
+++ src/main/java/ru/brominchik/lessons/files/BankWorker.java
@@ -3,34 +3,33 @@ package ru.brominchik.lessons.files;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.swing.filechooser.FileSystemView;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 
 public class BankWorker {
     private static final Logger logger = LoggerFactory.getLogger(BankWorker.class);
     private final Random random = new Random();
-    public File file;
-    private long baseAccount;
-    private long finalAccount;
 
-    public void createFile(int numOfOperations) throws IOException {
-        file = new File("C:\\test.txt");
-        FileOutputStream fileOutputStream = new FileOutputStream(file);
-        for (int i = 0; i < numOfOperations; i++) {
+    public File createFile(long numOfOperations, String path) throws IOException {
+        File file = new File(path);
+        FileOutputStream fileOutputStream = new FileOutputStream(path);
+        for (long i = 0; i < numOfOperations; i++) {
             int x = random.nextInt(200000) - 100000;
-            String baseString = x + " ";
+            String baseString = x + "\n ";
             fileOutputStream.write(baseString.getBytes());
         }
         fileOutputStream.close();
+        return file;
     }
 
-    public void doOperations() throws FileNotFoundException {
-        baseAccount = 1000000;
-        finalAccount = 1000000;
+    public void doOperations(File file, long baseAccount, long finalAccount) throws FileNotFoundException {
         Scanner scanner = new Scanner(file);
         while (scanner.hasNext()) {
             int sum = ((scanner.nextInt()));
@@ -47,17 +46,10 @@ public class BankWorker {
         scanner.close();
         boolean result = file.delete();
         if (result) {
-            System.out.println("File is successfully deleted.");
+            logger.info("File is successfully deleted.");
         } else {
-            System.out.println("File deletion failed.");
+            logger.info("File deletion failed.");
         }
     }
 
-    public long getBaseAccount() {
-        return baseAccount;
-    }
-
-    public long getFinalAccount() {
-        return finalAccount;
-    }
 }
--- src/test/java/ru/brominchik/lessons/files/BankWorkerTest.java
+++ src/test/java/ru/brominchik/lessons/files/BankWorkerTest.java
@@ -1,35 +1,40 @@
 package ru.brominchik.lessons.files;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BankWorkerTest {
     BankWorker bankWorker = new BankWorker();
 
     @Test
-    public void testForSomeAmount() throws IOException {
-        bankWorker.createFile(1000);
-        bankWorker.doOperations();
-
+    @Timeout(2)
+    public void testCreateFile() throws IOException {
+        File file = bankWorker.createFile(1000_000, "C:\\test.txt");
+        Scanner scanner = new Scanner(file);
+        long numOfStrings = 0;
+        while (scanner.hasNext()) {
+            int sum = ((scanner.nextInt()));
+            numOfStrings++;
+            assertTrue((sum <= 100000) & (sum >= -100000));
+        }
+        assertEquals(1000_000, numOfStrings);
     }
 
     @Test
-    public void testForDeletion() throws IOException {
-        bankWorker.createFile(1000);
-        bankWorker.doOperations();
-        assertFalse(bankWorker.file.exists());
+    public void testDoOperations() throws IOException {
+        File file = bankWorker.createFile(1000, "C:\\Users\\text.txt");
+        long baseAccount = 100000;
+        long finalAccount = 0;
+        bankWorker.doOperations(file, baseAccount, finalAccount);
+        assertEquals(100000, baseAccount + finalAccount);
+        assertFalse(file.exists());
     }
 
-    @Test
-    public void testForCorrectAnswer() throws IOException {
-        bankWorker.createFile(1000);
-        bankWorker.doOperations();
-        System.out.println(bankWorker.getBaseAccount());
-        System.out.println(bankWorker.getFinalAccount());
-        assertEquals(2000000, (bankWorker.getBaseAccount() + bankWorker.getFinalAccount()));
-    }
+
 }
