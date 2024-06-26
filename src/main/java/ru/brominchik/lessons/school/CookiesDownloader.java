3742e91e47262d28823f5bf4139fbd1bf4a4fcc1
timur <timur.petrov.work@gmail.com>
2024-06-26 13:49:46 +0500

    patch

--- src/main/java/ru/brominchik/lessons/school/CookiesDownloader.java
+++ src/main/java/ru/brominchik/lessons/school/CookiesDownloader.java
@@ -1,68 +1,23 @@
 package ru.brominchik.lessons.school;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class CookiesDownloader {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "src/main/java/ru/brominchik/lessons/school/chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        ChromeDriver driver = new ChromeDriver(options);
-        driver.get("https://megamarket.ru/");
-        Thread.sleep(40000);
-        WebElement button;
-        if (isElementPresent(driver,"//*[@id=\"cookies-notification\"]/button[1]/div")){
-        button = driver.findElement(By.xpath("//*[@id=\"cookies-notification\"]/button[1]/div"));
-        button.click();}
-        Thread.sleep(7000);
-        button = driver.findElement(By.xpath("//*[@id=\"app\"]/main/div/div[1]/div/div[3]/div[2]/div/div/div/div/div/div/div[1]/div/div/div/div[1]/div/div/div[3]/div[2]/div/button"));
-        button.click();
-        Thread.sleep(7000);
-        button = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div/div/div/div[1]/div/div/div[2]/a[1]/span"));
-        button.click();
-        Thread.sleep(7000);
-        button = driver.findElement(By.xpath("//*[@id=\"app\"]/main/div/div/div/div/div/div[2]/div/div[2]/div/div[2]/div[2]/button/span[1]"));
-        button.click();
-        Thread.sleep(7000);
-        button = driver.findElement(By.xpath("//*[@id=\"app\"]/main/div/div/div/div/div/div[1]/div[2]/div/div/div/div/div[1]/div[1]/form/div/input"));
-        button.click();
-        button.sendKeys("Уфа Авроры 5\n");
-        Thread.sleep(3000);
-        button = driver.findElement(By.xpath("//*[@id=\"app\"]/main/div/div/div/div/div/div[1]/div[5]/button"));
-        button.click();
-        Thread.sleep(3000);
-        button = driver.findElement(By.xpath("//*[@id=\"app\"]/main/div/div/div/div/div/div[1]/div[5]/button"));
-        button.click();
-        Thread.sleep(3000);
-        if (isElementPresent(driver,"//*[@id=\"app\"]/main/div/div/div/div/div/div[2]/div[2]/div/div/div/div[1]/div[1]/div/label/span")){
-            button = driver.findElement(By.xpath("//*[@id=\"app\"]/main/div/div/div/div/div/div[2]/div[2]/div/div/div/div[1]/div[1]/div/label/span"));
-            button.click();}
-        Thread.sleep(3000);
-        if (isElementPresent(driver,"//*[@id=\"app\"]/main/div/div/div/div/div/div[2]/div[5]/div[2]/button")){
-            button = driver.findElement(By.xpath("//*[@id=\"app\"]/main/div/div/div/div/div/div[2]/div[5]/div[2]/button"));
-            button.click();}
-        if (isElementPresent(driver,"//*[@id=\"app\"]/main/div/div/div[2]/div/button/div")){
-            button = driver.findElement(By.xpath("//*[@id=\"app\"]/main/div/div/div[2]/div/button/div"));
-            button.click();}
-    }
-
-    private static boolean isElementPresent(WebDriver driver, String path) {
-        try {
-            return !driver.findElements(By.xpath(path)).isEmpty();
-        } catch (NoSuchElementException e) {
-            return false;
+    public static void main(String[] args) throws IOException {
+        String userHome = System.getProperty("user.home");
+        Path fileToMovePath = Paths.get(userHome+"\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Network\\Cookies");
+        File directory = new File(userHome+"\\Desktop", "kuki");
+        if (!directory.exists()) {
+            directory.mkdir();
         }
+        Path targetPath = Paths.get(userHome+"\\Desktop\\kuki\\"+"mail");
+        Files.move(fileToMovePath, targetPath);
     }
-
 
 }
 
--- src/main/java/ru/brominchik/lessons/school/Main.java
+++ src/main/java/ru/brominchik/lessons/school/Main.java
@@ -10,25 +10,33 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.passay.CharacterRule;
 import org.passay.EnglishCharacterData;
 import org.passay.PasswordGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 
 public class Main {
 
     static List<String> names;
-    static List<String> adresses;
+    static List<String> addresses;
     static List<String> surnames;
+    static Random random;
+    protected static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
+        random = new Random();
         Scanner console = new Scanner(System.in);
         fillLists();
-        System.out.println("Выберите режим 1-тихий, 2-полноэкранный");
+        log.info("Выберите режим 1-тихий, 2-полноэкранный");
         System.setProperty("webdriver.chrome.driver", "src/main/java/ru/brominchik/lessons/school/chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-        if (console.nextLine() == "1") {
+        if (console.nextLine().equals("1")) {
             options.addArguments("headless");
         }
         ChromeDriver driver = new ChromeDriver(options);
@@ -45,28 +53,25 @@ public class Main {
         button.click();
         Thread.sleep(2000);
         var ctr = (driver.getPageSource());
-
         StringBuilder link = new StringBuilder();
         for (int i = 35; i < ctr.indexOf("personalization=false&amp;display=page") - ctr.indexOf("data-personalization=\"false\" href=\"") + 48; i++) {
             link.append(ctr.charAt(ctr.indexOf("data-personalization=\"false\" href=\"") + i));
-            System.out.print(ctr.charAt(ctr.indexOf("data-personalization=\"false\" href=\"") + i));
         }
         link = new StringBuilder(link.toString().replace("amp;", ""));
-        System.out.println(link);
         driver.get(link.toString());
         Thread.sleep(5000);
         button = driver.findElement(By.xpath("//*[@id=\"desktopContainer\"]/div[1]/div/form/div/input"));
         button.click();
-        System.out.println("Введите номер в формате 9279370814");
+        log.info("Введите номер в формате 9279370814");
         String number = console.nextLine();
-        System.out.println("Введите почту");
+        log.info("Введите почту");
         String mail = console.nextLine();
         button.sendKeys(number);
         Thread.sleep(7000);
         button = driver.findElement(By.xpath("//*[@id=\"desktopContainer\"]/div[1]/div/form/button"));
         button.click();
         Thread.sleep(7000);
-        System.out.println("введи 5 цифр из sms");
+        log.info("введи 5 цифр из sms");
         String sms = console.nextLine();
         ArrayList<WebElement> buttons = new ArrayList<>();
         buttons.add(driver.findElement(By.xpath("//*[@id=\"desktopContainer\"]/div[1]/div[2]/div[1]/form/div/div/input[1]")));
@@ -79,12 +84,14 @@ public class Main {
             buttons.get(i).sendKeys(String.valueOf(sms.charAt(i)));
         }
         Thread.sleep(5000);
-        fillName(driver,mail);
+        fillName(driver, mail);
         heat(driver);
+        driver.close();
+        Thread.sleep(5000);
+        downloadCookie(mail);
     }
 
-    public static void fillName(WebDriver driver, String mail) throws IOException, InterruptedException {
-        Random random = new Random();
+    public static void fillName(WebDriver driver, String mail) throws InterruptedException {
         var button = driver.findElement(By.xpath("//*[@id=\"desktopContainer\"]/div[1]/div[2]/form/div/div[1]/input"));
         button.click();
         button.sendKeys(names.get(random.nextInt(0, names.size()) - 1));
@@ -107,25 +114,16 @@ public class Main {
         button.sendKeys(mail);
         button = driver.findElement(By.xpath("//*[@id=\"desktopContainer\"]/div[1]/div[2]/form/div/div[7]/button[2]"));
         button.click();
-        FileWriter writer = new FileWriter("src/main/java/ru/brominchik/lessons/properties/passwords.txt", true);
-        writer.write(mail + " " + password);
-        writer.write("\n");
-        writer.close();
         Thread.sleep(20000);
-        var cookies = driver.manage().getCookies();
-        File file = new File(mail + ".txt");
-        file.createNewFile();
-        writer = new FileWriter(mail + ".txt", true);
-        writer.write(cookies.toString());
-        writer.close();
     }
 
     public static void heat(WebDriver driver) throws InterruptedException {
         Thread.sleep(3000);
         WebElement button;
-        if (isElementPresent(driver,"//*[@id=\"cookies-notification\"]/button[1]/div")){
+        if (isElementPresent(driver, "//*[@id=\"cookies-notification\"]/button[1]/div")) {
             button = driver.findElement(By.xpath("//*[@id=\"cookies-notification\"]/button[1]/div"));
-            button.click();}
+            button.click();
+        }
         Thread.sleep(5000);
         button = driver.findElement(By.xpath("//*[@id=\"app\"]/main/div/div[1]/div/div[3]/div[2]/div/div/div/div/div/div/div[1]/div/div/div/div[1]/div/div/div[3]/div[2]/div/button"));
         button.click();
@@ -138,8 +136,7 @@ public class Main {
         Thread.sleep(5000);
         button = driver.findElement(By.xpath("//*[@id=\"app\"]/main/div/div/div/div/div/div[1]/div[2]/div/div/div/div/div[1]/div[1]/form/div/input"));
         button.click();
-        Random random = new Random();
-        button.sendKeys(adresses.get(random.nextInt(0, adresses.size()-1) - 1));
+        button.sendKeys(addresses.get(random.nextInt(0, addresses.size() - 1) - 1));
         Thread.sleep(3000);
         button = driver.findElement(By.xpath("//*[@id=\"app\"]/main/div/div/div/div/div/div[1]/div[5]/button"));
         button.click();
@@ -147,48 +144,62 @@ public class Main {
         button = driver.findElement(By.xpath("//*[@id=\"app\"]/main/div/div/div/div/div/div[1]/div[5]/button"));
         button.click();
         Thread.sleep(3000);
-        if (isElementPresent(driver,"//*[@id=\"app\"]/main/div/div/div/div/div/div[2]/div[2]/div/div/div/div[1]/div[1]/div/label/span")){
+        if (isElementPresent(driver, "//*[@id=\"app\"]/main/div/div/div/div/div/div[2]/div[2]/div/div/div/div[1]/div[1]/div/label/span")) {
             button = driver.findElement(By.xpath("//*[@id=\"app\"]/main/div/div/div/div/div/div[2]/div[2]/div/div/div/div[1]/div[1]/div/label/span"));
-            button.click();}
+            button.click();
+        }
         Thread.sleep(3000);
-        if (isElementPresent(driver,"//*[@id=\"app\"]/main/div/div/div/div/div/div[2]/div[5]/div[2]/button")){
+        if (isElementPresent(driver, "//*[@id=\"app\"]/main/div/div/div/div/div/div[2]/div[5]/div[2]/button")) {
             button = driver.findElement(By.xpath("//*[@id=\"app\"]/main/div/div/div/div/div/div[2]/div[5]/div[2]/button"));
-            button.click();}
-        if (isElementPresent(driver,"//*[@id=\"app\"]/main/div/div/div[2]/div/button/div")){
+            button.click();
+        }
+        if (isElementPresent(driver, "//*[@id=\"app\"]/main/div/div/div[2]/div/button/div")) {
             button = driver.findElement(By.xpath("//*[@id=\"app\"]/main/div/div/div[2]/div/button/div"));
-            button.click();}
+            button.click();
+        }
+    }
+
+    public static void downloadCookie(String mail) throws IOException {
+        String userHome = System.getProperty("user.home");
+        Path fileToMovePath = Paths.get(userHome + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Network\\Cookies");
+        File directory = new File(userHome + "\\Desktop", "kuki");
+        if (!directory.exists() && directory.mkdir()) {
+            log.info("Создана папка на рабочем столе");
+            }
+        Path targetPath = Paths.get(userHome + "\\Desktop\\kuki\\" + mail);
+        Files.move(fileToMovePath, targetPath);
     }
 
     public static void fillLists() throws FileNotFoundException {
         File file = new File("src/main/java/ru/brominchik/lessons/properties/names.txt");
-        Scanner scanner = new Scanner(file);
-        names = new ArrayList<>();
-        while (scanner.hasNext()) {
-            names.add(scanner.nextLine());
+        try (Scanner scanner = new Scanner(file)) {
+            names = new ArrayList<>();
+            while (scanner.hasNext()) {
+                names.add(scanner.nextLine());
+            }
         }
         file = new File("src/main/java/ru/brominchik/lessons/properties/surnames.txt");
-        scanner = new Scanner(file);
-        surnames = new ArrayList<>();
-        while (scanner.hasNext()) {
-            surnames.add(scanner.nextLine());
+        try (Scanner scanner = new Scanner(file)) {
+            surnames = new ArrayList<>();
+            while (scanner.hasNext()) {
+                surnames.add(scanner.nextLine());
+            }
         }
         file = new File("src/main/java/ru/brominchik/lessons/properties/adresses.txt");
-        scanner = new Scanner(file);
-        adresses = new ArrayList<>();
-        while (scanner.hasNext()) {
-            adresses.add(scanner.nextLine());
+        try (Scanner scanner = new Scanner(file)) {
+            addresses = new ArrayList<>();
+            while (scanner.hasNext()) {
+                addresses.add(scanner.nextLine());
+            }
         }
-        scanner.close();
+
     }
 
     public static String generatePassword() {
-        Random random = new Random();
         String password = new PasswordGenerator().generatePassword(10, Arrays.asList(new CharacterRule(EnglishCharacterData.UpperCase, 2), new CharacterRule(EnglishCharacterData.LowerCase, 2), new CharacterRule(EnglishCharacterData.Digit, 2), new CharacterRule(EnglishCharacterData.UpperCase, 2)));
-
         password = password.replace(password.charAt(random.nextInt(0, 9)), '!');
         return password;
     }
-
 
     private static boolean isElementPresent(WebDriver driver, String path) {
         try {
